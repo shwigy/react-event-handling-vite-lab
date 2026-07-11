438c9094fb9184f462886b2f02d867315f4b12d3
shwigy <floressj04@icloud.com>
2026-07-11 17:57:34 -0400

    completed password feature

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,8 +1,13 @@
 // Code PasswordInput Component Here
 
 function PasswordInput (){
+
+    function handleChange() {
+        console.log("Entering password...");
+    }
+
     return(
-        <></>
+        <input type="password" onChange={handleChange} />
     )
 }
 
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,8 +1,17 @@
 // Code SubmitButton Component Here
 
 function SubmitButton (){
+
+    function handleEnter() {
+        console.log("Mouse Entering");
+    }
+
+    function handleLeave() {
+        console.log("Mouse Exiting");
+    }
+
     return(
-        <></>
+        <button onMouseEnter={handleEnter} onMouseLeave={handleLeave}>Submit Password</button>
     )
 }
 
